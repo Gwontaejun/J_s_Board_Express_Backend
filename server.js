4c846bb89038d3224fbd4667859f5fb7605f961e
Gwontaejun <hope10204@daum.net>
2021-03-16 22:59:36 +0900

    heroku hello world

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3002;
 
-// app.get('/', (req, res) => res.send("hello world"));
+app.get('/', (req, res) => res.send("hello world"));
 // app.listen(port, () => console.log(`Connect at http://localhost:${port}`));
 const cors = require("cors");
 const bodyParser = require("body-parser");
